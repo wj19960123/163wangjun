15ba9fd67e682afe313a3eb848dec696cbdf7614
wj19960123 <37889987+wj19960123@users.noreply.github.com>
2018-06-26 14:50:40 +0800

    Add files via upload

---
+++ 7.6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+{void concatenate(char string1[],char string2[],char string[]);
+ char s1[100],s2[100],s[100];
+ printf("input string1:");
+ scanf("%s",s1);
+ printf("input string2:");
+ scanf("%s",s2);
+ concatenate(s1,s2,s);
+ printf("\nThe new string is %s\n",s);
+ return 0;
+ }
+void concatenate(char string1[],char string2[],char string[])
+{int i,j;
+ for (i=0;string1[i]!='\0';i++)
+   string[i]=string1[i];
+ for(j=0;string2[j]!='\0';j++)
+   string[i+j]=string2[j];
+ string[i+j]='\0';
+}
+
